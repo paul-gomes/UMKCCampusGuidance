4e07c9d364552a9d71aee63b1047ad57278fe699
Anik Paul Gomes <gomesap29@gmail.com>
2020-02-29 04:36:11 -0600

    mapView added

--- UMKCCampusGuidance/src/app/tab2/tab2.page.ts
+++ UMKCCampusGuidance/src/app/tab2/tab2.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Map, tileLayer, marker } from "leaflet";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"]
 })
 export class Tab2Page {
-
+  map: Map;
   constructor() {}
 
+  ionViewDidEnter() {
+    this.loadMap();
+  }
+
+  loadMap(): void {
+    this.map = new Map("map").setView([39.034924, -94.578561], 25);
+    tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY- SA</a>'
+    }).addTo(this.map); // This line is added to add the Tile Layer to our map
+  }
 }
