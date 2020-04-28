c66eb5749a759d1d35ee8bfde802983f45e7e44b
Tanya Anisimova <anisimova.tanya07@gmail.com>
2020-04-28 11:35:07 -0500

    action sheet image source picker

--- UMKCCampusGuidance/src/app/upload-schedule/upload-schedule.page.ts
+++ UMKCCampusGuidance/src/app/upload-schedule/upload-schedule.page.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Camera, CameraOptions } from '@ionic-native/Camera/ngx';
+import {Component, OnInit} from '@angular/core';
+import {Camera, CameraOptions} from '@ionic-native/Camera/ngx';
+import {OCRResult} from '@ionic-native/ocr';
+import {ActionSheetController} from '@ionic/angular';
+
 // import { File } from '@ionic-native/file/ngx';
 
 @Component({
@@ -8,47 +11,63 @@ import { Camera, CameraOptions } from '@ionic-native/Camera/ngx';
   styleUrls: ['./upload-schedule.page.scss'],
 })
 export class UploadSchedulePage implements OnInit {
-  text = '';
+  text: OCRResult;
   message = '';
+  selectedImage;
+  imageData;
 
   imagePickerOptions = {
     maximumImagesCount: 1,
     quality: 50
   };
 
-  constructor(private camera: Camera) { }
+  constructor(private camera: Camera,
+              private actionSheetController: ActionSheetController) { }
 
   ngOnInit() { }
 
-  takePicture() {
-    this.pickImage(this.camera.PictureSourceType.CAMERA);
-  }
-
-  chooseImage() {
-    this.pickImage(this.camera.PictureSourceType.PHOTOLIBRARY);
+  selectSource() {
+    this.actionSheetController.create({
+      buttons: [
+        {
+          text: 'Use Library',
+          handler: () => {
+            this.pickImage(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        }, {
+          text: 'Capture Image',
+          handler: () => {
+            this.pickImage(this.camera.PictureSourceType.CAMERA);
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    }).then(ac => ac.present());
   }
 
   pickImage(source) {
-    this.text = 'Loading';
     const options: CameraOptions = {
       quality: 100, sourceType: source,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true
     };
     this.camera.getPicture(options).then((imageData) => {
-      this.text = imageData;
-      // this.httpService.getText(imageData).subscribe((data) => {
-      //   if (data['OCRText'] === 0) {
-      //     this.text = '';
-      //     this.message = data['OCRErrorMessage'];
-      //     return;
-      //   }
-      //   this.message = '';
-      //   this.text = data['OCRText'][0];
-      // });
+      this.imageData = imageData;
+      this.selectedImage = `data:image/jpeg;base64,${imageData}`;
     }, (err) => {
       this.message = err.toString();
     });
+  }
+
+  useImage() {
+    // this.ocr.recText(OCRSourceType.NORMFILEURL, this.imageData).then((result: OCRResult) => {
+    //   this.text = result;
+    // }).catch((error) => {
+    //   console.log(error);
+    // });
   }
 }
